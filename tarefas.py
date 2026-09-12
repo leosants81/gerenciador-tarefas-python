21d788720f51bead801ff943867ea4104f9bcb57
Leonardo Santos <leosants81@gmail.com>
2026-09-12 12:50:09 -0300

    Permite adicionar varias tarefas e criado o metodo concluir tarefas

--- tarefas.py
+++ tarefas.py
@@ -2,26 +2,39 @@ class Tarefas:
 
     def __init__(self):
         self.tarefas = []
+        self.tarefas_concluidas = []
 
-    def adicionar_tarefa(self, tarefa):
-        if tarefa.strip() != "":
-            self.tarefas.append(tarefa)
-            return True
-        return False
+    def adicionar_tarefa(self, *tarefas):
+        tarefa_adicionada = []
+        for tarefa in tarefas:
+            if tarefa.strip() != "":
+                self.tarefas.append(tarefa)
+                tarefa_adicionada.append(tarefa)
+        return tarefa_adicionada
+
 
     def remover_tarefas(self, indice):
         if 0 <= indice < len(self.tarefas):
             self.tarefas.pop(indice)
 
+
+    def concluir_tarefa(self, indice):
+        if 0 <= indice < len(self.tarefas):
+            tarefa = self.tarefas.pop(indice)
+            self.tarefas_concluidas.append(tarefa)
+            return tarefa
+        return None
+    
+
     def listar_tarefas(self):
         return self.tarefas
 
 
+
 tf1 = Tarefas()
-tf1.adicionar_tarefa('desenhar')
-tf1.adicionar_tarefa('estudar')
-tf1.adicionar_tarefa('academia')
+tf1.adicionar_tarefa('estudar', 'tomar banho', 'arrumar para o trabalho')
+
 tf1.remover_tarefas(0)
-tf1.adicionar_tarefa('trabalhar')
+
 
 print(tf1.listar_tarefas())
