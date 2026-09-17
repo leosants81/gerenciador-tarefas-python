912d931f77a9503ed3503651a09b42352d0bf65d
Leonardo Santos <leosants81@gmail.com>
2026-09-16 22:47:46 -0300

    melhora no gerenciamento de tarefas e tratamento de erros

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from tarefas import Tarefas
 
 
+def adicionar_tarefa_segura(tf, *tarefa):
+    '''tartando erro caso a tarefa nao seja um texto '''
+    try:
+        tarefa_adicionada = tf.adicionar_tarefa(*tarefa)
+        print(f'Tarefa "{tarefa_adicionada}" adicionada')
+        return tarefa_adicionada
+    except TypeError as e:
+        print(f'Error {e}')
+        return None
+
+
 def remover_tarefa_segura(tf, indice):
     '''removendo uma tarefa e tratando erro caso indice seja invalido'''
     try:
-        tf.remover_tarefa(indice)
-        print(f'tarefa no indice {indice} removida')
-        return True
+        tarefa = tf.remover_tarefa(indice)
+        print(f'tarefa "{tarefa}" removida')
+        return tarefa
     except IndexError as e:
         print(f'Error {e}')
-        return False
+        return None
 
 
 def concluir_tarefa_segura(tf, indice):
@@ -24,9 +35,16 @@ def concluir_tarefa_segura(tf, indice):
 
 
 tf = Tarefas()
-tf.adicionar_tarefa('estudar', 'ler', 'dormir')
-remover_tarefa_segura(tf, 0)      
-concluir_tarefa_segura(tf, 0)   
+adicionadas = tf.adicionar_tarefa(
+    'estudar', 'ler', 'dormir', 'trabalhar', 'jogar')
+print('tarefas adicionadas', adicionadas)
+print('tarefas pendentes', tf.listar_tarefas())
 
-print('tarefa pendente', tf.listar_tarefas())       
-print('tarefa concluida', tf.listar_tarefas_concluidas())
+
+print('teste de erro com int')
+adicionar_tarefa_segura(tf,'estudar',123,'dormir')
+
+concluir_tarefa_segura(tf, 0)
+remover_tarefa_segura(tf, 1)
+print('Pendentes:', tf.listar_tarefas())
+print('Concluídas:', tf.listar_tarefas_concluidas())
--- tarefas.py
+++ tarefas.py
@@ -7,40 +7,40 @@ class Tarefas:
     def adicionar_tarefa(self, *tarefas):
         tarefa_adicionada = []
         for tarefa in tarefas:
+            if not isinstance(tarefa, str):
+                mensagem = (
+                    f'Tarefa deve ser um texto, '
+                    f'recebido: {type(tarefa).__name__}'
+                )
+                raise TypeError(mensagem)
             if tarefa.strip() != "":
                 self.tarefas.append(tarefa)
                 tarefa_adicionada.append(tarefa)
         return tarefa_adicionada
 
-
     def remover_tarefa(self, indice):
         if not (0 <= indice < len(self.tarefas)):
             mensagem = (
-            f'indice inválido: {indice}. '
-            f'Existem {len(self.tarefas)} tarefa(s).'
-        )
+                f'indice inválido: {indice}. '
+                f'Existem {len(self.tarefas)} tarefa(s).'
+            )
             raise IndexError(mensagem)
-        self.tarefas.pop(indice)
-
-
+        tarefa_removida = self.tarefas.pop(indice)
+        return tarefa_removida
 
     def concluir_tarefa(self, indice):
         if not (0 <= indice < len(self.tarefas)):
             mensagem = (
-            f'indice inválido: {indice}. '
-            f'Existem {len(self.tarefas)} tarefa(s).'
-        )
+                f'indice inválido: {indice}. '
+                f'Existem {len(self.tarefas)} tarefa(s).'
+            )
             raise IndexError(mensagem)
         tarefa = self.tarefas.pop(indice)
         self.tarefas_concluidas.append(tarefa)
         return tarefa
 
-
     def listar_tarefas(self):
-        return self.tarefas
-
+        return self.tarefas.copy()
 
     def listar_tarefas_concluidas(self):
-        return self.tarefas_concluidas
-
-
+        return self.tarefas_concluidas.copy()
